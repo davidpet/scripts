eda0d979d94ccb1a823574982aaefc6375a721b2
David Petrofsky <davidpet@ucla.edu>
2023-02-12 18:02:49 -0800

    Adding conda and jupyter convenience aliases.

--- Bash/config/bashrc-mac.sh
+++ Bash/config/bashrc-mac.sh
@@ -1,3 +1,8 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 export PATH=/usr/local/bin:$PATH
+
+alias jupyter-tutorials="jupyter notebook --notebook-dir=~/repos/tutorials/Jupyter"
+
+alias ai="conda activate ai"
+
