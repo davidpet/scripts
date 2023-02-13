fe098b484178db00a66843e61f4320f0b257afcb
David Petrofsky <davidpet@ucla.edu>
2023-02-12 18:09:21 -0800

    Adding convenience aliases for jupyter and conda.

--- Bash/config/bashrc-linux.sh
+++ Bash/config/bashrc-linux.sh
@@ -9,3 +9,9 @@ function dos2unix() {
 # Get rid of Jupyter kernel errors in WSL
 export JUPYTER_ALLOW_INSECURE_WRITES=true
 
+# Quickly switch to AI environment.
+alias ai="conda activate ai"
+
+# Run Jupyter notebooks in tutorials folder with no browser so I can hit it from Windows.
+alias jupyter-tutorials="jupyter notebook --notebook-dir=~/repos/tutorials/Jupyter --no-browser"
+
