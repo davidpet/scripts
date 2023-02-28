e1ba01f729040bb316cea2452ea4118b9709a22e
David Petrofsky <davidpet@ucla.edu>
2023-02-28 13:55:38 -0800

    Adding jupyter folder for code snippets.

--- Bash/config/bashrc-mac.sh
+++ Bash/config/bashrc-mac.sh
@@ -4,5 +4,7 @@ export PATH=/usr/local/bin:$PATH
 
 alias jupyter-tutorials="jupyter notebook --notebook-dir=~/repos/tutorials/Jupyter"
 
+alias jupyter-snippets="jupyter notebook --notebook-dir=~/repos/snippets"
+
 alias ai="conda activate ai"
 
