b052cbbcaf8eeb3c5fc07be002f4e7660f0e0b02
David Petrofsky <davidpet@ucla.edu>
2023-05-14 20:57:21 -0700

    Adding alias for running ml-models repo in jupyter notebook environment.

--- Bash/config/bashrc-linux.sh
+++ Bash/config/bashrc-linux.sh
@@ -24,6 +24,9 @@ alias jupyter-snippets="jupyter notebook --notebook-dir=~/repos/snippets --no-br
 # Run Jupyter notebooks in Course Labs folder with no browser.
 alias jupyter-coursera="jupyter notebook --notebook-dir=/mnt/p/Training/Coursera\ Courses --no-browser"
 
+# Run Jupyter notebooks in ml-models folder with no browser.
+alias jupyter-models="jupyter notebook --notebook-dir=~/repos/ml-models --no-browser"
+
 # Look for occurences of the given regex pattern within jupyter notebooks from coursera labs.
 function grep-coursera() {
   grep -R --exclude-dir='\.ipynb_checkpoints' --include '*.ipynb' "$1" "/mnt/p/Training/Coursera Courses"
